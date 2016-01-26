dda17a4617a5b0a7d24c38b6bab5cd0d49c666ba
dongja94 <dongja94@gmail.com>
2016-01-26 10:45:39 +0900

    switcher

--- app/src/main/java/com/example/dongja94/sampletransition/MainActivity.java
+++ app/src/main/java/com/example/dongja94/sampletransition/MainActivity.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextSwitcher switcher;
+    String[] items = "Displays text to the user and optionally allows them to edit it".split(" ");
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switcher = (TextSwitcher)findViewById(R.id.textSwitcher);
+
         Button btn = (Button)findViewById(R.id.btn_other);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -19,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_change);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView)switcher.getNextView();
+                textView.setText(items[index % items.length]);
+                index++;
+                switcher.showNext();
             }
         });
     }
